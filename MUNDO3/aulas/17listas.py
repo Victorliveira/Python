2b42980c526b010d0569858a516cd692c9145947
Victor Oliveira <victor.soliveira03@gmail.com>
2026-03-01 20:48:44 -0300

    aula 16
    listas

---
+++ MUNDO3/aulas/17listas.py
@@ -0,0 +1,32 @@
+# listas s]ao feitas com []
+
+lanche = ['burguer','suco','pizza','pudim']
+print(lanche)
+lanche[3] = 'picole' # listas são MUTÁVEIS
+print(lanche)
+
+lanche.append('cookie') # append adiciona um elemento no final da lista
+print(lanche)
+lanche.insert(2,'sushi')# insert adiciona um elemento na chave desejada
+print(lanche)
+
+del lanche[3] # del serve para eliminar um elemento indicando sua chave
+print(lanche)
+lanche.pop(3)# por padrão elimina o último elemento, mas pode ser parametrizado para eliminar a chave desejada
+print(lanche)
+lanche.remove('sushi') # o método remove usa o VALOR invés da chave
+print(lanche)
+
+valores = list(range(4,11)) # criando uma lista usando range
+print(valores)
+
+num =[8,2,5,3,9,3,0]
+num.sort()# deixa os elementos da lista em ordem
+num.sort(reverse=True)# deixa a lista em ordem ao contrário
+
+lista = [2,4,3,7]
+lisb = lista[:] # copia os valores
+lisb[2] = 8
+print(lista)
+print(lisb)
+
---
+++ MUNDO3/desafios/078.py
@@ -0,0 +1,19 @@
+# Programa que leia 5 valores num e guarde-os em uma lista. No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.
+
+num =[]
+for v in range(0,5):
+    num.append(int(input(f'Digite um valor para a posição {v}: ')))
+
+print(f'Você digitou os valores {num}')
+
+print(f'O valor {max(num)} foi o maior digitado e está nas posições ', end='')
+for pos,v in enumerate(num):
+    if v== max(num):
+        print(f' {pos}...', end='')
+
+
+print(f'\nO valor {min(num)} foi o menor digitado e está nas posições', end='')
+for pos,v in enumerate(num):
+    if v == min(num):
+        print(f' {pos}...', end='')
+
