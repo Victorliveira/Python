9320727577afa9b1542852e9199e8ecbd7f251db
Victor Oliveira <133777590+Victorliveira@users.noreply.github.com>
2026-03-18 19:56:56 -0300

    ex83
    ex83 finalizado

--- MUNDO3/desafios/083.py
+++ MUNDO3/desafios/083.py
@@ -1,2 +1,24 @@
 #Programa onde o usuário digite uma expressão qlq que use parênteses.Seu app deverá analisar se a expressão passada está com os parênteses abertos e fechados na ordem correta.
 
+
+exp = input("Digite uma expressão algoritimíca: ")
+
+expv = []
+
+for c in exp:
+    if c == '(':
+        expv.append(c)
+    if c == ')':
+        if expv:
+            expv.pop()    
+        else:
+            expv.append(c)
+            break
+
+
+if not expv:
+    print('Expressão correta')
+else:
+    print('Expressão incorreta')
+
+    
