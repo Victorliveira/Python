8e1f2819ecc758444220ef66b36de5dca082c17d
Victor Oliveira <133777590+Victorliveira@users.noreply.github.com>
2026-03-25 17:57:44 -0300

    ex85
    realizado ex85

--- MUNDO3/desafios/085.py
+++ MUNDO3/desafios/085.py
@@ -1,2 +1,12 @@
 #Programa onde o usuário possa digitar 7 valores num e cadastre-os em uma lista única que mantenha separados os valores pares e ímpares. No final, mostre os valores pares e ímpares em ordem crescente. 
 
+dados = [[],[]]
+for v in range(1,8):
+    num = int(input(f'Digite {v}° valor : '))
+    if num %2 ==0:
+        dados[0].append(num)
+    else:
+        dados[1].append(num)
+
+print(f'Os valores pares digitados foram: ', sorted(dados[0]) )
+print(f'Os valores ímpares digitados foram: ', sorted(dados[1]))
