fad8cb4922058e5e25b88fd2a4f99a6a2ceeb183
Victor Oliveira <133777590+Victorliveira@users.noreply.github.com>
2026-03-25 21:46:19 -0300

    ex 86 e 87
    finalizado o ex86 e ex87 em andamento

--- MUNDO3/desafios/086.py
+++ MUNDO3/desafios/086.py
@@ -1,1 +1,24 @@
 # Programa que crie uma matriz de dimensão 3x3 e preencha com valores lidos pelo teclado. No final, mostre a matriz na tela, com a formatação correta
+
+linha1 = list()
+linha2 = list()
+linha3 = list()
+matriz = list()
+
+matriz.append(linha1)
+matriz.append(linha2)
+matriz.append(linha3)
+cont= 0
+
+# cadastra o número em todos os parâmetros
+while True:
+    for v in range(1,4):
+        matriz[cont].append(int(input(f'Digite um número para a linha {cont+1} coluna {v}: ')))    
+    cont+=1
+    if cont ==3:
+        break
+
+#printa em forma de matriz
+for linha in matriz:
+    print(linha)
+    
--- MUNDO3/desafios/087.py
+++ MUNDO3/desafios/087.py
@@ -1,1 +1,26 @@
 # Aprimore o desafio 86, mostrando no final: a- soma de todos os valores pares digitados. b- soma dos valores da terceira coluna. c- O maior valor da segunda linha
+
+matriz = []
+#tamanho da matriz
+tmatriz = 3
+somap = somac = 0
+
+for l in range(tmatriz):
+    linha = []
+    for v in range(1,4):
+        linha.append(int(input(f'Digite um número para a linha {l+1} da coluna {v}: ')))
+    matriz.append(linha)
+
+
+for c in matriz:
+    for num in c:
+        if num %2 == 0:
+            somap += num
+
+#printa a matriz        
+for c in matriz:
+    print(c)
+
+print(f'A soma de todos os números pares é igual a {somap}')
+print(f'O maior valor da linha 2 é {max(matriz[1])}')
+print(f'A soma da terceira coluna é igual a  ')
